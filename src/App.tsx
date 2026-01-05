2cc10c2adb07242413ffefac5a0c4698c57fbdc8
Warren Brown <warrengbrn@gmail.com>
2026-01-05 03:55:57 -0500

    removed query

--- src/App.tsx
+++ src/App.tsx
@@ -13,59 +13,59 @@ import { Compare } from './sections/compare';
     const whereClause = `Admin_Filter IN ('gadm0', 'gadm1')`;
     var queryString = `where=${encodeURIComponent(whereClause)}`;
 
-    async function test() {
+    // async function test() {
 
-      const url = `https://services9.arcgis.com/weJ1QsnbMYJlCHdG/arcgis/rest/services/Floods_riverine_people_all/FeatureServer/0/query?${queryString}&outFields=*&f=json&returnIdsOnly=true`;
-      const result = await fetch(url);
-      var x = await result.json();
+    //   const url = `https://services9.arcgis.com/weJ1QsnbMYJlCHdG/arcgis/rest/services/Floods_riverine_people_all/FeatureServer/0/query?${queryString}&outFields=*&f=json&returnIdsOnly=true`;
+    //   const result = await fetch(url);
+    //   var x = await result.json();
 
-      var y = x.objectIds.filter((value: ObjectID) => value);
-      console.log(y);
-      console.log(y.join(","));
+    //   var y = x.objectIds.filter((value: ObjectID) => value);
+    //   console.log(y);
+    //   console.log(y.join(","));
       
-      var y2;
+    //   var y2;
 
-      var count = 1;
+    //   var count = 1;
 
-      type SliceNumber = {
-        start: number,
-        end: number
-      };
+    //   type SliceNumber = {
+    //     start: number,
+    //     end: number
+    //   };
 
-      function counter({start, end}: SliceNumber) {
-        y2 = y.slice(start, end);
+    //   function counter({start, end}: SliceNumber) {
+    //     y2 = y.slice(start, end);
 
-        const params = new URLSearchParams({
-          objectIds: y2.join(","),
-          outFields: '*',
-          f: 'json'
-        });
+    //     const params = new URLSearchParams({
+    //       objectIds: y2.join(","),
+    //       outFields: '*',
+    //       f: 'json'
+    //     });
 
-        fetch('https://services9.arcgis.com/weJ1QsnbMYJlCHdG/arcgis/rest/services/Floods_riverine_people_all/FeatureServer/0/query', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          },
-          body: params
-        })
-          .then(res => res.json())
-          .then(data => {
-            console.log(data);
-            if (end < y.length) {
-              count += 2000;
-              counter({ start: 0 + count, end: 2000 + count});
+    //     fetch('https://services9.arcgis.com/weJ1QsnbMYJlCHdG/arcgis/rest/services/Floods_riverine_people_all/FeatureServer/0/query', {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/x-www-form-urlencoded'
+    //       },
+    //       body: params
+    //     })
+    //       .then(res => res.json())
+    //       .then(data => {
+    //         console.log(data);
+    //         if (end < y.length) {
+    //           count += 2000;
+    //           counter({ start: 0 + count, end: 2000 + count});
 
-            }
-          });
-      }
+    //         }
+    //       });
+    //   }
 
-      counter({ start: 0, end: 2000});
-    }
+    //   counter({ start: 0, end: 2000});
+    // }
 
-    test();
+    // test();
 
 
-  const [currentView, setView] = useState("Compare");
+  const [currentView, setView] = useState("Grid");
   const [currentDimension, setDimension] = useState("2D");
   const [currentTime, setTime] = useState({ time: "1980-2014", url: "https://tiles.arcgis.com/tiles/weJ1QsnbMYJlCHdG/arcgis/rest/services/riverine_flood_grid_people_historical_1980/VectorTileServer" });
 
